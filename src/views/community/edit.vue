672c9d3b1fafd406d936596a3d1a71e4cee7a106
宋凯翔 <304250210@qq.com>
2020-11-22 22:07:44 +0800

    图片上传功能（差回显）

--- src/views/community/edit.vue
+++ src/views/community/edit.vue
@@ -103,12 +103,7 @@ export default {
      * 上传图片方法
      */
     beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg'
       const isLt2M = file.size / 1024 / 1024 < 2
-      if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!')
-        return false
-      }
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 2MB!')
         return false
